68b9fa0af273884ddc903295c671f4f52f371053
11 <probabble@gmail.com>
2020-07-14 16:21:52 -0700

    dumb

--- src/hooks.js
+++ src/hooks.js
@@ -7,7 +7,6 @@ export const useRoundware = () => useContext(RoundwareContext)
 
 export const useDeviceID = () => {
   const [cookies, setCookie, removeCookie] = useCookies(['deviceId']);
-  removeCookie("deviceId");
 
   if (!cookies['deviceId']) {
     const millisecondsYear = 365 * 24 * 60 * 60 * 1000;
