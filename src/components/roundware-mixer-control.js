09403ec77dc8a6a7c2fa23414016322a58ec8b00
Halsey Burgund <halsey@halseyburgund.com>
2021-01-24 13:40:53 -0500

    add skip button to transport controls on Listen page; need to add dynamic enable/disable features

--- src/components/roundware-mixer-control.js
+++ src/components/roundware-mixer-control.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 import Button from "@material-ui/core/Button";
 import {useRoundware} from "../hooks";
 import { GeoListenMode } from "roundware-web-framework";
@@ -17,37 +18,50 @@ const RoundwareMixerControl = props => {
   }}, [])
 
   return (
-    <Button
-      onClick={() => {
-        if (!roundware._mixer) {
-          roundware
-            .activateMixer({ geoListenMode: GeoListenMode.MANUAL })
-            .then((token, force) => {
-              const listen_tags = roundware.uiConfig.listen[0].display_items.map(
-                (i) => i.tag_id
-              );
-              roundware._mixer.updateParams({
-                listenerLocation: roundware._listenerLocation,
-                minDist: 0,
-                maxDist: 0,
-                recordingRadius: 0,
-                listenTagIds: listen_tags,
+    <>
+      <Button
+        onClick={() => {
+          if (!roundware._mixer) {
+            roundware
+              .activateMixer({ geoListenMode: GeoListenMode.MANUAL })
+              .then((token, force) => {
+                const listen_tags = roundware.uiConfig.listen[0].display_items.map(
+                  (i) => i.tag_id
+                );
+                roundware._mixer.updateParams({
+                  listenerLocation: roundware._listenerLocation,
+                  minDist: 0,
+                  maxDist: 0,
+                  recordingRadius: 0,
+                  listenTagIds: listen_tags,
+                });
+                roundware._mixer.toggle(token, force);
+                forceUpdate();
               });
-              roundware._mixer.toggle(token, force);
-              forceUpdate();
-            });
-        } else {
-          roundware._mixer.toggle(roundware._mixer.token, false);
-          forceUpdate();
-        }
-      }}
-    >
-      {roundware && roundware._mixer && roundware._mixer.playing ? (
-        <PauseCircleOutlineIcon fontSize="large" />
-      ) : (
-        <PlayCircleOutlineIcon fontSize="large" />
-      )}
-    </Button>
+          } else {
+            roundware._mixer.toggle(roundware._mixer.token, false);
+            forceUpdate();
+          }
+        }}
+      >
+        {roundware && roundware._mixer && roundware._mixer.playing ? (
+          <PauseCircleOutlineIcon fontSize="large" />
+        ) : (
+          <PlayCircleOutlineIcon fontSize="large" />
+        )}
+      </Button>
+      <Button
+        onClick={() => {
+          if (!roundware._mixer) {
+
+          } else {
+            roundware._mixer.skipTrack(7);
+          }
+        }}
+      >
+        <SkipNextIcon />
+      </Button>
+    </>
   );
 };
 
