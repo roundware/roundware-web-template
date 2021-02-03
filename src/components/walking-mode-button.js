6cf15a9d82c936a9c8d00c5164207dd8dc9e325d
Halsey Burgund <halsey@halseyburgund.com>
2021-02-03 12:19:48 -0500

    experimenting with getting location updates from framework to re-center map

--- src/components/walking-mode-button.js
+++ src/components/walking-mode-button.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRoundware } from "../hooks";
+import { GeoListenMode } from "roundware-web-framework";
 import { useGoogleMap } from "@react-google-maps/api";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
@@ -23,6 +24,22 @@ const walkingModeButton = () => {
   const map = useGoogleMap();
   const classes = useStyles();
   const [walkingMode, setwalkingMode] = useState(false);
+  const loc = roundware._listenerLocation
+  const lat = loc && loc.latitude
+  const lng = loc && loc.longitude
+  const center = { lat, lng }
+  const ready = typeof(lat) === "number" && typeof(lng) === "number"
+
+  // when the listenerLocation is updated, center the map
+  useEffect(() => {
+    if (ready) {
+      const c = map.getCenter();
+      console.log("new location provided by framework");
+      if (center.lat !== c.lat() || center.lng !== c.lng()) {
+        map.panTo(center)
+      }
+    }
+  }, [lat, lng])
 
   const toggleWalkingMode = () => {
     if (walkingMode) {
@@ -39,8 +56,10 @@ const walkingModeButton = () => {
       // zoom in
       map.setZoom(16);
       // change to geoListenMode.AUTOMATIC
+      roundware.enableGeolocation(GeoListenMode.AUTOMATIC)
       // make project.recordingRadius circle invisible
       // update text instructions?
+      // use spinner to indicate location is being determined initially
     }
     setwalkingMode(!walkingMode);
   };
