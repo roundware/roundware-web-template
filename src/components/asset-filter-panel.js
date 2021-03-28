fa08a9eeaafcfccfd8eb540fbfc0969be4a9bbae
Halsey Burgund <halsey@halseyburgund.com>
2021-03-28 14:59:47 -0400

    add snackbar notification on filter updates

--- src/components/asset-filter-panel.js
+++ src/components/asset-filter-panel.js
@@ -1,10 +1,16 @@
 import { useRoundware } from "../hooks";
-import React from "react";
+import React, { useState } from "react";
 import { DebounceInput } from "react-debounce-input";
 import Grid from "@material-ui/core/Grid";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -15,11 +21,21 @@ const useStyles = makeStyles(theme => ({
 export const TagFilterMenu = ({ tag_group }) => {
   const classes = useStyles();
   const { selectTags, selectedTags } = useRoundware();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
 
   const handleChange = (evt, value, action, target) => {
     const tag_ids = value ? value.map((t) => t.value) : null;
     selectTags(tag_ids, tag_group);
+    setSnackbarOpen(true);
   };
+
   const options = tag_group.display_items.map(
     ({ tag_id, tag_display_text }) => {
       return {
@@ -28,33 +44,46 @@ export const TagFilterMenu = ({ tag_group }) => {
       };
     }
   );
+
   const fieldId = `roundware-tag-${tag_group.header_display_text}`;
 
   const selectedTagGroupTags = selectedTags[tag_group.group_short_name] || []
 
   return (
-    <Grid item xs={12} className={`tag-filter-field tag-filter-select`}>
-      <label className="tag-filter-field--label">
-        <Autocomplete
-          multiple
-          id={tag_group.name}
-          classes={classes}
-          options={options}
-          getOptionLabel={(option) => option ? option.label: ""}
-          onChange={handleChange}
-          getOptionSelected={option => selectedTagGroupTags.indexOf(option.value) !== -1}
-          value={options.filter(o => selectedTagGroupTags.indexOf(o.value) !== -1)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="standard"
-              label={tag_group.header_display_text}
-              placeholder="Select one or more..."
-            />
-          )}
-        ></Autocomplete>
-      </label>
-    </Grid>
+    <>
+      <Grid item xs={12} className={`tag-filter-field tag-filter-select`}>
+        <label className="tag-filter-field--label">
+          <Autocomplete
+            multiple
+            id={tag_group.name}
+            classes={classes}
+            options={options}
+            getOptionLabel={(option) => option ? option.label: ""}
+            onChange={handleChange}
+            getOptionSelected={option => selectedTagGroupTags.indexOf(option.value) !== -1}
+            value={options.filter(o => selectedTagGroupTags.indexOf(o.value) !== -1)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="standard"
+                label={tag_group.header_display_text}
+                placeholder="Select one or more..."
+              />
+            )}
+          ></Autocomplete>
+        </label>
+      </Grid>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={4000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success">
+          Success! Filters updated.
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
