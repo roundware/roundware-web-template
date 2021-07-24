72418a25ba1063378983149093928f64f81600cf
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-07-24 13:45:53 +0530

    Support TypeScript

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import React from 'react';
+import { App } from 'components/App';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { RoundwareProvider } from './providers/RoundwareProvider';
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(
+	<RoundwareProvider>
+		<BrowserRouter>
+			<App style={{ display: 'flex' }} />
+		</BrowserRouter>
+	</RoundwareProvider>,
+	mountNode
+);
