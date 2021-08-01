1c485e3ebb2d0eff671d04120cb59da0800e7118
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-01 19:17:21 +0530

    show warning when API key not passed

--- src/components/ListenPage.tsx
+++ src/components/ListenPage.tsx
@@ -14,6 +14,10 @@ const useStyles = makeStyles((theme) => {
 const ListenPage = () => {
 	const classes = useStyles();
 
+	if (!process.env.GOOGLE_MAPS_API_KEY) {
+		console.warn(`GOOGLE_MAPS_API_KEY was not found in env variable. Please pass it to enable Google Maps component.`);
+		return null;
+	}
 	return <RoundwareMap className={classes.map} googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY} />;
 };
 
