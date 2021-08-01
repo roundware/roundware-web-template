3aac6464526834dfe0aa2fd1a6bea7e23e6de8b2
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-01 19:14:39 +0530

    make value optional and asset return value

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,1 @@
-export const wait = (delay, value) =>
-  new Promise(resolve => setTimeout(resolve, delay, value));
+export const wait = <PromiseType>(delay: number, value?: any): Promise<PromiseType> => new Promise((resolve) => setTimeout(resolve, delay, value));
