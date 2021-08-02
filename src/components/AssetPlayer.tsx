d8b5dfef68a03cbf8f192c7119e4cdbab1a243a5
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 15:23:05 +0530

    converted to typescript

--- src/components/AssetPlayer.tsx
+++ src/components/AssetPlayer.tsx
@@ -1,37 +1,36 @@
-import React from "react";
+import React, { CSSProperties } from 'react';
+interface AssetPlayerProps {
+	asset: any;
+	style?: CSSProperties;
+	className?: string;
+}
+interface AssetPlayerState {}
+class AssetPlayer extends React.Component<AssetPlayerProps, AssetPlayerState> {
+	render() {
+		const { asset } = this.props;
+		if (!asset) {
+			return null;
+		}
 
-class AssetPlayer extends React.Component {
-  render() {
-    const { asset } = this.props;
-    if (!asset) {
-      return null;
-    }
+		let ext = /(?:\.([^.]+))?$/.exec(asset.file)![1];
+		let filename = asset.file;
+		const supported = ['mp3', 'wav'];
+		if (supported.indexOf(ext) === -1) {
+			ext = 'mp3';
+			let lastPos = filename.indexOf('.', filename.length - 5);
+			let pos = lastPos == -1 ? filename.length : lastPos;
+			filename = filename.substr(0, pos < 0 ? filename.length : pos) + '.' + ext;
+		}
 
-    let ext = (/(?:\.([^.]+))?$/).exec(asset.file)[1]
-    let filename = asset.file
-    const supported = ['mp3', 'wav'];
-    if (supported.indexOf(ext) === -1) {
-      ext = "mp3"
-      let lastPos = filename.indexOf(".", filename.length - 5);
-      let pos = (lastPos == -1) ? filename.length : lastPos;
-      filename = filename.substr(0, pos < 0 ? filename.length : pos) + "." + ext;
-    }
+		const audioType = `audio/${ext}`;
 
-    const audioType = `audio/${ext}`
-
-    return (
-      <audio
-        controls
-        style={this.props.style}
-        preload="none"
-        controlsList="nodownload"
-        className={this.props.className}
-      >
-        <source src={filename} type={audioType} />
-        Your browser does not support audio!
-      </audio>
-    );
-  }
+		return (
+			<audio controls style={this.props.style} preload='none' controlsList='nodownload' className={this.props.className}>
+				<source src={filename} type={audioType} />
+				Your browser does not support audio!
+			</audio>
+		);
+	}
 }
 
 export default AssetPlayer;
