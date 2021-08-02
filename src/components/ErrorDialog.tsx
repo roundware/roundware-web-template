18baad32932a6cb4d4db2de7468fbc7c6311a6ef
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 15:17:15 +0530

    Added a default message something went wrong

--- src/components/ErrorDialog.tsx
+++ src/components/ErrorDialog.tsx
@@ -1,28 +1,32 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import React from 'react';
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import Typography from "@material-ui/core/Typography";
 
-const ErrorDialog = ({error, set_error}) => {
-
-    return (
-      <Dialog open={error !== null} >
-      <DialogContent>
-        <DialogContentText>
-          { error && error.message }
-        </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button
-                variant="contained"
-                onClick={()=>{set_error(null)}}
-            >OK</Button>
-        </DialogActions>
-      </Dialog>
-    )
+interface ErrorDialogProps {
+	error: Error | null;
+	set_error: React.Dispatch<React.SetStateAction<Error | null>>;
 }
+const ErrorDialog = ({ error, set_error }: ErrorDialogProps) => {
+	return (
+		<Dialog open={error !== null}>
+			<DialogContent>
+				<DialogContentText>{error ? error.message : `Something went wrong! Please try again.`}</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button
+					variant='contained'
+					onClick={() => {
+						set_error(null);
+					}}
+				>
+					OK
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
 
 export default ErrorDialog;
