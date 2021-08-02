af232e982d09085223084a1c74cc1c1981304f2d
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 15:23:36 +0530

    conversion to typescript

--- src/components/AssetTags.tsx
+++ src/components/AssetTags.tsx
@@ -1,29 +1,32 @@
-import { useRoundware } from "../hooks";
-import React from "react";
+import { useRoundware } from '../hooks';
+import React from 'react';
 
-export const TagDisplay = ({ tagId }) => {
-  const { roundware } = useRoundware();
-  const description = roundware.findTagDescription(tagId, "speak");
-  if (description) {
-    return (
-      <>
-        <span className="rw-tag">{description}</span>
-        <br />
-      </>
-    )
-  } else {
-    return null;
-  }
+interface TagDisplayProps {
+	tagId: unknown;
+}
+export const TagDisplay = ({ tagId }: TagDisplayProps) => {
+	const { roundware } = useRoundware();
+	const description = roundware.findTagDescription(tagId, 'speak');
+	if (description) {
+		return (
+			<>
+				<span className='rw-tag'>{description}</span>
+				<br />
+			</>
+		);
+	} else {
+		return null;
+	}
 };
 
-export const TagsDisplay = ({ tagIds }) => {
-  return (
-    <div className="rw-tags">
-      {tagIds.map((tagId) => (
-        <React.Fragment key={tagId}>
-          <TagDisplay tagId={tagId} />
-        </React.Fragment>
-      ))}
-    </div>
-  );
+export const TagsDisplay = ({ tagIds }: { tagIds: unknown[] }) => {
+	return (
+		<div className='rw-tags'>
+			{tagIds.map((tagId: any) => (
+				<React.Fragment key={tagId}>
+					<TagDisplay tagId={tagId} />
+				</React.Fragment>
+			))}
+		</div>
+	);
 };
