a904cfdf8dca10021c434c345700b3ee071238eb
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 15:21:51 +0530

    Converted to typescript, sortEntries not implemented

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -1,55 +1,48 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import AssetListItem from "./asset-list-item";
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useRoundware } from '../hooks';
+import AssetFilterPanel from './AssetFilterPanel';
+import AssetListItem from './AssetListItem';
 
-import { useRoundware } from "../hooks";
-import AssetFilterPanel from "./asset-filter-panel";
+interface AssetListProps {
+	assets: any[];
+}
+const AssetList = (props: AssetListProps) => {
+	const { roundware, selectAsset, selectedAsset, filteredAssets }: any = useRoundware();
+	const [minimized, set_minimized] = useState(false);
+	const [showing_filters, show_filters] = useState(false);
 
-const AssetList = (props) => {
-  const {
-    roundware,
-    selectAsset,
-    selectedAsset,
-    filteredAssets,
-  } = useRoundware();
-  const [minimized, set_minimized] = useState(false);
-  const [showing_filters, show_filters] = useState(false);
-
-  let assets;
-  if (!roundware) {
-    assets = props.assets || [];
-  } else {
-    assets = filteredAssets || props.assets || [];
-  }
-  const entries = assets.sort(sortEntries).map((asset) => {
-    return <AssetListItem key={asset.id} asset={asset} player={false} />;
-  });
-  return (
-    <div className="asset-list">
-      <div className="asset-list--header">
-        <div className="header-controls">
-          <button
-            className={`minimizeButton fa fa-${minimized ? "expand-alt" : "compress-alt"
-              }`}
-            onClick={() => set_minimized(!minimized)}
-          />
-          <button
-            className="showFiltersButton fa fa-filter"
-            onClick={() => show_filters(!showing_filters)}
-          />
-        </div>
-        <AssetFilterPanel hidden={!showing_filters} />
-      </div>
-      <div className={`asset-list--assets ${minimized ? "hidden" : ""}`}>
-        {entries}
-      </div>
-    </div>
-  );
+	let assets;
+	if (!roundware) {
+		assets = props.assets || [];
+	} else {
+		assets = filteredAssets || props.assets || [];
+	}
+	// sortEntries is not implemented
+	const entries = assets.sort(sortEntries).map((asset: any) => {
+		return <AssetListItem key={asset.id} asset={asset} player={false} />;
+	});
+	return (
+		<div className='asset-list'>
+			<div className='asset-list--header'>
+				<div className='header-controls'>
+					<button className={`minimizeButton fa fa-${minimized ? 'expand-alt' : 'compress-alt'}`} onClick={() => set_minimized(!minimized)} />
+					<button className='showFiltersButton fa fa-filter' onClick={() => show_filters(!showing_filters)} />
+				</div>
+				<AssetFilterPanel hidden={!showing_filters} />
+			</div>
+			<div className={`asset-list--assets ${minimized ? 'hidden' : ''}`}>{entries}</div>
+		</div>
+	);
 };
 
 AssetList.propTypes = {
-  assets: PropTypes.arrayOf(Object),
-  roundware: PropTypes.object,
+	assets: PropTypes.arrayOf(Object),
+	roundware: PropTypes.object,
 };
 
 export default AssetList;
+
+function sortEntries(sortEntries: any) {
+	throw new Error('Function not implemented.');
+}
