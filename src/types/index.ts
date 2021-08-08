7be9a02f5c05378f9cb4f591c0ac8b23d2bd39dc
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-08 18:00:23 +0530

    reusable types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,9 @@
+import { ITag } from 'roundware-web-framework/dist/types';
+
 // all the reusable types here
+export interface ITagLookup {
+	[id: number]: ITag;
+}
+export interface ISelectedTags {
+	[group_key: string]: number[]; // tag_ids
+}
