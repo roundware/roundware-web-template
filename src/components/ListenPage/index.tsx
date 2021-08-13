fd34a51544b23a16c006b6bc33c9747b6feb70fb
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-13 21:51:45 +0530

    Early asset loading overlay

--- src/components/ListenPage/index.tsx
+++ src/components/ListenPage/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import RoundwareMap from './Map';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { useQuery, useRoundware } from '../../hooks';
+import AssetLoadingOverlay from './Map/AssetLoadingOverlay';
 
 const useStyles = makeStyles((theme) => {
 	return {
@@ -18,7 +19,12 @@ const ListenPage = () => {
 		console.warn(`GOOGLE_MAPS_API_KEY was not found in env variable. Please pass it to enable Google Maps component.`);
 		return null;
 	}
-	return <RoundwareMap className={classes.map} googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY} />;
+	return (
+		<>
+			<AssetLoadingOverlay />
+			<RoundwareMap className={classes.map} googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY} />
+		</>
+	);
 };
 
 export default ListenPage;
