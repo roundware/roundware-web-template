00667d79526c42ec9a06d1861484fb723bcaee81
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-21 12:31:31 +0530

    sync tag_ids with URL

--- src/providers/URLProvider.tsx
+++ src/providers/URLProvider.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useHistory } from 'react-router';
 import { IAssetData } from 'roundware-web-framework/dist/types';
+import { IRoundwareContext } from '../context/RoundwareContext';
 
 import { IURLContext, URLContext } from '../context/URLContext';
 import { useRoundware } from '../hooks';
@@ -9,37 +10,76 @@ import { useRoundware } from '../hooks';
 export const URLSyncProvider = ({ children }: { children: React.ReactNode }) => {
 	const location = useLocation();
 	const history = useHistory();
-	const { selectAsset, assetPage, selectedAsset } = useRoundware();
-
-	// all the params for query
-	const [params, setParams] = useState<IURLContext[`params`]>(new URLSearchParams());
-	useEffect(() => {
-		setParams(new URLSearchParams(location.search));
-	}, [location]);
-
-	// when params change get the individual params
-	useEffect(() => {
-		setEid(parseInt(params.get('eid') || '') || null);
-	}, [params]);
+	const { roundware, selectAsset, assetPage, selectedAsset, selectedTags, selectTags } = useRoundware();
 
 	// envelope id passed in GET
-	const [eid, setEid] = useState<number | null>(null);
+	const [eid_get, setEid_get] = useState<number | null>(null);
+	const [loading, setLoading] = useState(true);
+	// when it's first time
 	useEffect(() => {
-		if (eid) {
-			const asset = assetPage.find((a) => a?.envelope_ids?.indexOf(eid) !== -1);
-			asset ? selectAsset(asset) : selectAsset(null);
-		} else {
-			selectAsset(null);
-		}
-	}, [eid, assetPage]);
+		if (!Array.isArray(roundware?.uiConfig?.listen)) return;
+		// when listen tag_ids are loaded
+		// select the tag_ids which are passed in URL
+		const params = new URLSearchParams(location.search);
+		const tids = params.get('tag_ids');
+		if (tids) {
+			const get_tagIds = tagStringToArray(tids);
+			setTag_ids_get(get_tagIds);
+			if (Array.isArray(get_tagIds)) {
+				roundware.uiConfig.listen.forEach((tg) => {
+					// look if the tag_ids are relevant and belong to which group
+					if (Array.isArray(tg.display_items)) {
+						let tag_ids_group: number[] = [];
+						tg.display_items.forEach((item) => {
+							if (get_tagIds.includes(item.tag_id)) tag_ids_group.push(item.tag_id);
+						});
+						if (tag_ids_group.length > 0) {
+							selectTags(tag_ids_group, tg);
+						}
+					}
+				});
+			}
+		} else setTag_ids_get(null);
+		setLoading(false);
+	}, [roundware?.uiConfig?.listen]);
 
 	useEffect(() => {
-		if (eid !== null && selectedAsset == null) deleteFromURL('eid');
-		if (eid == null && selectedAsset && Array.isArray(selectedAsset.envelope_ids)) addToURL('eid', selectedAsset.envelope_ids[0].toString());
+		const params = new URLSearchParams(location.search);
+		setEid_get(parseInt(params.get('eid') || '') || null);
+	}, [location.search]);
+
+	useEffect(() => {
+		if (eid_get) {
+			const asset = assetPage.find((a) => a?.envelope_ids?.indexOf(eid_get) !== -1);
+			asset ? selectAsset(asset) : selectAsset(null);
+		} else selectAsset(null);
+	}, [eid_get, assetPage]);
+
+	useEffect(() => {
+		if (eid_get !== null && selectedAsset == null) deleteFromURL('eid');
+		if (eid_get == null && selectedAsset && Array.isArray(selectedAsset.envelope_ids)) addToURL('eid', selectedAsset.envelope_ids[0].toString());
 	}, [selectedAsset]);
 
+	// add and remove tag_ids
+	const [tag_ids_get, setTag_ids_get] = useState<number[] | null>();
+
+	useEffect(() => {
+		if (loading) return; // let the updating of tag_ids finish
+		const params = new URLSearchParams(location.search);
+		// check if URL is already synced with selectedTags
+		if (JSON.stringify(tagStringToArray(params.get('tag_ids'))) == JSON.stringify(tagStringToArray(selectedTagsToURLString(selectedTags)))) {
+			// no need to update the URL
+			return;
+		}
+		if (selectedTags !== null) {
+			addToURL('tag_ids', selectedTagsToURLString(selectedTags)!);
+		} else deleteFromURL('tag_ids');
+	}, [selectedTags]);
+
 	const addToURL = (name: string, value: string) => {
-		const newParams = params;
+		const newParams = new URLSearchParams(location.search);
+		// remove previous value
+		if (newParams.get(name)) newParams.delete(name);
 		newParams.append(name, value);
 		history.push({
 			pathname: window.location.pathname,
@@ -47,7 +87,7 @@ export const URLSyncProvider = ({ children }: { children: React.ReactNode }) => 
 		});
 	};
 	const deleteFromURL = (name: string) => {
-		const newParams = params;
+		const newParams = new URLSearchParams(location.search);
 		newParams.delete(name);
 		history.push({
 			pathname: window.location.pathname,
@@ -55,5 +95,25 @@ export const URLSyncProvider = ({ children }: { children: React.ReactNode }) => 
 		});
 	};
 
-	return <URLContext.Provider value={{ params, addToURL, deleteFromURL }}>{children}</URLContext.Provider>;
+	return <URLContext.Provider value={{ params: new URLSearchParams(location.search), addToURL, deleteFromURL }}>{children}</URLContext.Provider>;
+};
+
+const selectedTagsToURLString = (selectedTags: IRoundwareContext[`selectedTags`]): string | null => {
+	if (selectedTags == null) return null;
+	let tagString = '';
+	let tags: number[] = [];
+	Object.values(selectedTags).forEach((tag_group) => tag_group.forEach((tag) => tags.push(tag)));
+	tags.sort((a, b) => a - b);
+	tags.forEach((t) => (tagString = tagString + t + ','));
+	if (tagString.length < 1) return null;
+	return tagString.slice(0, -1);
+};
+
+const tagStringToArray = (tagString: null | string): number[] | null => {
+	if (tagString == null || tagString.toString().length == 0) return null;
+	const tagArray = tagString
+		.toString()
+		.split(',')
+		.map((id) => parseInt(id));
+	return tagArray.sort((a, b) => a - b);
 };
