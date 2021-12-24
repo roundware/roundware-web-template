099028d2cf6d1a12ca1bb4c1ce9062bfab1b044b
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-12-24 19:08:24 +0530

    use new autocomplete

--- src/components/AssetFilterPanel/TagFilterMenu.tsx
+++ src/components/AssetFilterPanel/TagFilterMenu.tsx
@@ -1,11 +1,10 @@
-import { Grid, TextField } from '@mui/material';
-import Autocomplete, { AutocompleteChangeDetails, AutocompleteRenderInputParams } from '@mui/lab/Autocomplete';
+import { Grid, SnackbarCloseReason, SnackbarProps, TextField } from '@mui/material';
+import Autocomplete, { AutocompleteProps, AutocompleteChangeDetails, AutocompleteRenderInputParams } from '@mui/material/Autocomplete';
 import React, { useState, useContext } from 'react';
 import { useRoundware } from '../../hooks';
 import useStyles from './styles';
 import MuiAlert, { AlertProps } from '@mui/lab/Alert';
 import { Snackbar } from '@mui/material';
-import { AutocompleteChangeReason } from '@mui/lab';
 import { TextFieldProps } from 'material-ui';
 import { ITag, ITagGroup, IUiConfig } from 'roundware-web-framework/dist/types';
 import { URLContext } from '../../context/URLContext';
@@ -21,12 +20,18 @@ const TagFilterMenu = ({ tag_group }: TagFilterMenuProps) => {
 	const { roundware, selectTags, selectedTags } = useRoundware();
 	const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
 
-	const handleSnackbarClose = (event: React.ChangeEvent<{}>, reason?: string) => {
-		if (reason && reason === 'clickaway') return;
+	const handleSnackbarClose: SnackbarProps[`onClose`] = (event, reason) => {
+		if (reason == 'clickaway') return;
 		setSnackbarOpen(false);
 	};
 
-	const handleChange = (event: React.ChangeEvent<{}>, value: { value: number }[], reason: AutocompleteChangeReason, details?: AutocompleteChangeDetails<any> | undefined) => {
+	const handleChange = (
+		event: React.SyntheticEvent<Element, Event>,
+		value: {
+			value: number;
+			label: string;
+		}[]
+	) => {
 		const tag_ids = value ? value.map((t) => t.value) : null;
 
 		selectTags(tag_ids, tag_group);
@@ -50,18 +55,18 @@ const TagFilterMenu = ({ tag_group }: TagFilterMenuProps) => {
 
 	const selectedTagGroupTags = (tag_group && tag_group.group_short_name && selectedTags && selectedTags[tag_group.group_short_name]) || [];
 
-	return <>
-        <Grid item xs={12} className={`tag-filter-field tag-filter-select`}>
-            <label className='tag-filter-field--label'>
-                <Autocomplete multiple id={tag_group?.group_short_name} classes={classes} options={options} getOptionLabel={(option) => (option ? option.label : '')} onChange={handleChange} isOptionEqualToValue={(option) => selectedTagGroupTags?.indexOf(option.value) !== -1} value={options.filter((o: { value: number }) => selectedTagGroupTags.indexOf(o.value) !== -1)} renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} variant='standard' label={tag_group.header_display_text} placeholder='Select one or more...' />} />
-            </label>
-        </Grid>
-        <Snackbar open={snackbarOpen} autoHideDuration={4000} onClose={handleSnackbarClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
-            <Alert onClose={handleSnackbarClose} severity='success'>
-                Success! Filters updated.
-            </Alert>
-        </Snackbar>
-    </>;
+	return (
+		<>
+			<Grid item xs={12} className={`tag-filter-field tag-filter-select`}>
+				<label className='tag-filter-field--label'>
+					<Autocomplete multiple id={tag_group?.group_short_name} classes={classes} options={options} getOptionLabel={(option) => (option ? option.label : '')} onChange={handleChange} isOptionEqualToValue={(option) => selectedTagGroupTags?.indexOf(option.value) !== -1} value={options.filter((o: { value: number }) => selectedTagGroupTags.indexOf(o.value) !== -1)} renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} variant='standard' label={tag_group.header_display_text} placeholder='Select one or more...' />} />
+				</label>
+			</Grid>
+			<Snackbar open={snackbarOpen} autoHideDuration={4000} onClose={handleSnackbarClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+				<Alert severity='success'>Success! Filters updated.</Alert>
+			</Snackbar>
+		</>
+	);
 };
 
 export default TagFilterMenu;
