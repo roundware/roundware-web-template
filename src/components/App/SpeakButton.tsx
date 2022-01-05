82fb00c732567036e58071ec3ce036179a35a44c
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-05 19:54:25 +0530

    wrap with box

--- src/components/App/SpeakButton.tsx
+++ src/components/App/SpeakButton.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import MicIcon from '@mui/icons-material/Mic';
+import Box from '@mui/material/Box';
 interface Props {}
 
 const SpeakButton = (props: Props) => {
 	return (
-		<IconButton title='Speak'>
-			<MicIcon />
-		</IconButton>
+		<Box>
+			<IconButton title='Speak'>
+				<MicIcon />
+			</IconButton>
+		</Box>
 	);
 };
 
