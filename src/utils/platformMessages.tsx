e922fe2b72199d7a8872fdf33768cc8d8dc8d3a9
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-02-28 19:38:49 +0530

    functions to get messages

---
+++ src/utils/platformMessages.tsx
@@ -0,0 +1,34 @@
+import Link from '@mui/material/Link';
+import { PlatformMessage } from 'components/PlatformMessage';
+import { isIOS, isFirefox, isSafari } from 'react-device-detect';
+
+export function getMessageOnLoad(): PlatformMessage | null {
+	switch (true) {
+		// message
+		case isSafari:
+			return {
+				message: 'You are using Safari. Please use another browser',
+			};
+		// with custom title
+		case isFirefox:
+			return {
+				title: 'Custom title',
+				message: 'You are using Firefox. Please use another browser',
+			};
+
+		// with custom link and component
+		case isIOS:
+			return {
+				message: (
+					<span>
+						In order to get the best experience with Glam, we recommend that iOS users download the mobile app:
+						<Link href='https://appstore.com'>appstore.com</Link>
+					</span>
+				),
+				link: 'appstore.com',
+			};
+
+		default:
+			return null;
+	}
+}
