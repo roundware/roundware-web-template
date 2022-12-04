898231b25c4ebe3f95579f440db48797ab94bf0d
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-12-04 17:26:04 +0530

    contact info

--- src/components/SpeakPage/CreateRecordingForm/AdditionalMediaMenu/ContactInfo.tsx
+++ src/components/SpeakPage/CreateRecordingForm/AdditionalMediaMenu/ContactInfo.tsx
@@ -1,11 +1,12 @@
 import { Email } from '@mui/icons-material';
-import { Box, Button, ListItemIcon, ListItemText, MenuItem, Stack, TextField } from '@mui/material';
+import { Alert, Box, Button, ListItemIcon, ListItemText, MenuItem, Stack, TextField } from '@mui/material';
 import Modal from 'components/elements/Modal';
 import { useRoundware } from 'hooks';
 import config from 'config.json';
 import { useRef, useState } from 'react';
 import ApiClient from 'roundware-web-framework/dist/api-client';
 import { User } from 'roundware-web-framework/dist/user';
+import { LoadingButton } from '@mui/lab';
 
 type Props = {};
 
@@ -16,6 +17,8 @@ const ContactInfo = (props: Props) => {
 	const lastNameRef = useRef<HTMLInputElement>(null);
 	const emailRef = useRef<HTMLInputElement>(null);
 	const { roundware } = useRoundware();
+	const [loading, setLoading] = useState(false);
+
 	return (
 		<>
 			<MenuItem onClick={() => setOpen(true)}>
@@ -28,16 +31,29 @@ const ContactInfo = (props: Props) => {
 				<form
 					onSubmit={(e) => {
 						e.preventDefault();
+						setLoading(true);
+						roundware.user
+							.updateUser({
+								first_name: firstNameRef.current?.value,
+								last_name: lastNameRef.current?.value,
+								email: emailRef.current?.value,
+							})
+							.then(() => {
+								setOpen(false);
+							})
+							.finally(() => {
+								setLoading(false);
+							});
 					}}
 				>
-					<Stack spacing={1}>
+					<Stack spacing={2} minWidth='300px'>
 						<TextField label='First Name' name='first_name' required autoFocus tabIndex={100} ref={firstNameRef} />
 						<TextField label='Last Name' name='last_name' required tabIndex={101} ref={lastNameRef} />
 						<TextField label='Email' type='email' name='email' required tabIndex={102} ref={emailRef} />
 						<Box>
-							<Button type='submit' variant='contained'>
+							<LoadingButton loading={loading} type='submit' variant='contained'>
 								Submit
-							</Button>
+							</LoadingButton>
 						</Box>
 					</Stack>
 				</form>
