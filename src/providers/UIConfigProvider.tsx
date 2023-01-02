03aaa010aaefb2ba1a9174ba92492434b4fd889f
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2023-01-02 18:49:34 +0530

    active && defualtOpen

--- src/providers/UIConfigProvider.tsx
+++ src/providers/UIConfigProvider.tsx
@@ -15,7 +15,7 @@ const UiConfigProvider = ({ children }: { children: React.ReactNode }) => {
 	const isListenPage = useMemo(() => pathname.includes(`/listen`), [pathname]);
 
 	useEffect(() => {
-		if (isListenPage) setDrawerOpen(finalConfig.ui.listenSidebar.defaultOpen);
+		if (isListenPage) setDrawerOpen(finalConfig.ui.listenSidebar.active && finalConfig.ui.listenSidebar.defaultOpen);
 		else setDrawerOpen(false);
 	}, [isListenPage]);
 
