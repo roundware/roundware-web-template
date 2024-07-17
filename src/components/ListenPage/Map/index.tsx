fcf765e8d345d5cad329877c5d16418cae6bdb02
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2024-07-17 23:56:36 +0530

    out of range message

--- src/components/ListenPage/Map/index.tsx
+++ src/components/ListenPage/Map/index.tsx
@@ -18,6 +18,7 @@ import ResetButton from './ResetButton';
 import SpeakerImages from './Speakers/SpeakerImages';
 import SpeakerToggle from '../SpeakerToggle';
 import PlaybackInfoOverlay from '../PlaybackInfoOverlay';
+import OutOfRangeMessage from './OutOfRangeMessage';
 
 const useStyles = makeStyles((theme) => {
 	return {
@@ -153,6 +154,8 @@ const RoundwareMap = (props: RoundwareMapProps) => {
 								}}
 							/>
 						)}
+
+						<OutOfRangeMessage />
 					</GoogleMap>
 				</LoadScript>
 			) : null}
